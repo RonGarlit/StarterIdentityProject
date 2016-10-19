65d5d0da3145d98b3916d54c16b5609ab5b43cd9
RonGarlit <rongarlit@live.com>
2016-10-19 14:39:48 -0400

    Initial commit of project

---
+++ devproapp/Areas/Home/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DevProApp.Controllers
+{
+    [RouteArea("Home", AreaPrefix = "Home")]
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        [Route("~/")]
+        [Route]
+        [Route("Index")]
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [Route("~/Home/About")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        [Route("~/Home/Contact")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [Route("~/Home/Error")]
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        [Route("~/Home/UnderConstruction")]
+        public ActionResult UnderConstruction()
+        {
+            return View();
+        }
+    }
+}
